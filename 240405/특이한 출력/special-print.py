3afbb60dbdbb5149c0163ce46bcdd0cbdeb35f32
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-04-05 15:03:24 +0900

    Title: 특이한 출력, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240405/특이한 출력/special-print.py
+++ 240405/특이한 출력/special-print.py
@@ -3,5 +3,5 @@ n = int(input())
 for i in range(1,n+1):
     for j in range(1,n+1):
         print("({}, {})".format(i,j),end=' ')
-        if i+j == 4:
+        if (i+j) % 4 == 0:
             print()
