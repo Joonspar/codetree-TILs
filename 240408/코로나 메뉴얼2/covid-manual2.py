b3a8786f5e130c0e11c9052d5c5336b96acc9354
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-04-08 17:33:14 +0900

    Title: 코로나 메뉴얼2, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240408/코로나 메뉴얼2/covid-manual2.py
@@ -0,0 +1,14 @@
+cnt = [0] * 4
+for i in range(3):
+    a,b = input().split()
+    if a == 'Y' and int(b) >= 37:
+        cnt[0] += 1
+    elif a == 'N' and int(b) >= 37:
+        cnt[1] += 1
+    elif a == 'Y' and int(b) < 37:
+        cnt[2] += 1
+    elif a == 'N' and int(b) < 37:
+        cnt[3] += 1    
+print(*cnt,end=' ')
+if cnt[0] >= 2:
+    print('E')
