2a17e33b9e7a68935c363be7a65dc5684f14c237
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-04-08 12:00:18 +0900

    Title: 숫자들의 배수, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240408/숫자들의 배수/multiple-of-numbers.py
+++ 240408/숫자들의 배수/multiple-of-numbers.py
@@ -1,10 +1,8 @@
 n = int(input())
 cnt = 0
 i = 1
-while cnt <3:
-    print(i*n,end=' ')
-    i += 1
+while cnt < 2:
     if (i*n) % 5 == 0:
         cnt += 1
-    elif cnt == 2:
-        break
+    print(i*n,end=' ')
+    i += 1
