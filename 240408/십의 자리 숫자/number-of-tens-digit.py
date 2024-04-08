fc8f1d82f57c58849582e3a95874f58d8064a423
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-04-08 17:17:16 +0900

    Title: 십의 자리 숫자, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240408/십의 자리 숫자/number-of-tens-digit.py
+++ 240408/십의 자리 숫자/number-of-tens-digit.py
@@ -1,7 +1,19 @@
-li = list(map(int,input().split()))
-for i in range(1,10):
-    cnt = 0
-    for elem in li:
-        if (elem//10) == i:
-            cnt += 1
-    print("{} - {}".format(i,cnt))
+# 입력 받기
+numbers = list(map(int, input().split()))
+
+# 각 십의 자리의 숫자를 저장할 딕셔너리 생성
+count = {}
+
+# 0이 나올 때까지 입력된 수의 십의 자리 수 카운트
+for num in numbers:
+    if num == 0:
+        break
+    tens_digit = num // 10
+    if tens_digit in count:
+        count[tens_digit] += 1
+    else:
+        count[tens_digit] = 1
+
+# 결과 출력
+for i in range(1, 10):
+    print(f"{i} - {count.get(i, 0)}")
