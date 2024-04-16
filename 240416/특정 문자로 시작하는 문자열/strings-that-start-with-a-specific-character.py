c7bb134642c9ab060c9bb9aa95c1eeb9327f67a9
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-04-16 14:40:25 +0900

    Title: 특정 문자로 시작하는 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240416/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.py
@@ -0,0 +1,14 @@
+n = int(input())
+li = []
+cnt = 0
+res = 0
+for _ in range(4):
+    s = input()
+    li.append(s)
+c = input()
+
+for i in li:
+    if i[0] == c:
+        cnt += 1
+        res += len(i)
+print(cnt,"%.2f" %(res/cnt))
