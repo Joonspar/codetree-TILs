15e9d1c1d222df3023d12b624d453b2b037d8b60
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-05-16 14:21:07 +0900

    Title: 중앙값 계산 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240516/중앙값 계산 2/get-median-2.py
+++ 240516/중앙값 계산 2/get-median-2.py
@@ -1,12 +1,14 @@
 n=int(input())
 arr=list(input().split())
 arr2=[]
-
-for i in range(n):
-    arr2.append(arr[i])
-    arr2.sort()
-    lgth=len(arr2)
-    if (i+1)%2==1:
+if n == 0:
+    print(0)
+else:
+    for i in range(n):
+        arr2.append(arr[i])
+        arr2.sort()
+        lgth=len(arr2)
+        if (i+1)%2==1:
         # 중앙값 인덱스 = (lgth+1)/2 -1
-        mid = int((lgth+1)/2 -1)
-        print(arr2[mid],end=" ")
+            mid = int((lgth+1)/2 -1)
+            print(arr2[mid],end=" ")
