9213f50d1ec9d742969598635fe19c8e7bee2aa5
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-05-20 17:46:01 +0900

    Title: 연속되는 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240520/연속되는 수 2/continuous-number2.py
+++ 240520/연속되는 수 2/continuous-number2.py
@@ -4,10 +4,10 @@ for _ in range(n):
     arr.append(int(input()))
 cnt = 1
 arr2 = []
-for i in range(n):
-    if i == 0 or arr[i] == arr[i-1]:
+for i in range(1,n):
+    if arr[i] == arr[i-1]:
         cnt += 1
-    else:
+    elif i == 0 or arr[i] != arr[i-1]: 
         arr2.append(cnt)
         cnt = 1
 print(max(arr2))
