a79cdee88e787097a127b54961857454f306de77
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-05-21 12:45:53 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240521/만나는 그 순간/the-moment-we-meet.py
+++ 240521/만나는 그 순간/the-moment-we-meet.py
@@ -26,7 +26,6 @@ for _ in range(m):
         for _ in range(distance):
             cntb += 1
             brr.append(cntb)
-ans = 0
 for i in range(1,len(arr)):
     if arr[i] == brr[i]:
         ans = i
