c1f273ec4f0aa3df64a0e575f214f96fb6c56083
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-05-22 14:59:32 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

--- 240522/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
+++ 240522/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
@@ -5,9 +5,9 @@ res = 0
 for i in range(n):
     for j in range(n-1):
         s = 0
-        if j == i+1 or j == i-1:
+        if i == j+1 or i == j or i == j-1:
             continue
-        s = arr[j] + arr[i] 
+        s = arr[j] + arr[i]
         res = max(s,res)
 res = max(s,res)
 print(res)
