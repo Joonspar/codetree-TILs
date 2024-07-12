b73f2aa8e05227a0b4af76c0cc79a89a51ac7d7d
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-07-12 19:59:30 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240712/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.py
+++ 240712/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.py
@@ -2,8 +2,13 @@ n = int(input())
 first = 2
 second = 4
 res = 1
-for i in range(n-2):
-    res = (first * second) % 100
-    first = second
-    second = res
-print(res)
+if n == 1:
+    print(2)
+elif n == 2:
+    print(4)
+else:    
+    for i in range(n-2):
+        res = (first * second) % 100
+        first = second
+        second = res
+    print(res)
