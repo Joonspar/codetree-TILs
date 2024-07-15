58a698bd7e4b893c687a638d3c9c5e28b14c6779
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-07-15 14:04:08 +0900

    Title: 왔다 갔던 구역 2, Time: 175ms, Memory: 28MB, Status: Passed - CodeTree

--- 240715/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240715/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,19 +1,20 @@
 n = int(input())
-state = 10  # 초기 위치를 10으로 설정
-arr = [0] * 100  # 위치를 기록할 배열, 충분히 큰 크기로 설정
+current = n * 10
+arr = [0] * 2000
+for i in range(n):
+    x, s = input().split()
+    x = int(x)
+    if s == 'R':
+        for j in range(current+1, current + x +1 ):
+            arr[j] += 1
+        current += x
+    elif s == 'L':
+        for j in range(current, current - x, -1):
+            arr[j] += 1
+        current -= x
 
-for _ in range(n):
-    cnt, direction = input().split()
-    cnt = int(cnt)
-    if direction == 'R':
-        for i in range(1, cnt + 1):
-            arr[state + i] += 1
-        state += cnt
-    elif direction == 'L':
-        for i in range(1, cnt + 1):
-            arr[state - i] += 1
-        state -= cnt
-
-# 2번 이상 지나간 영역의 크기 계산
-overlapped_count = sum(1 for x in arr if x >= 2)
-print(overlapped_count)
+cnt = 0
+for i in range(len(arr)):
+    if arr[i] >= 2:
+        cnt += 1
+print(cnt)
