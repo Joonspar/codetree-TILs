ecd0c17306d20ddd755dc94fbcc4d66eaf2764fb
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-07-15 13:27:10 +0900

    Title: 왔다 갔던 구역 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240715/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240715/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,28 +1,30 @@
 def count_overlapped_areas(n, commands):
     position = 0
     positions = {}
-    
+
     for command in commands:
         steps, direction = command.split()
         steps = int(steps)
         
         if direction == 'R':
-            for _ in range(steps):
-                position += 1
-                if position in positions:
-                    positions[position] += 1
+            for i in range(1, steps + 1):
+                new_position = position + i
+                if new_position in positions:
+                    positions[new_position] += 1
                 else:
-                    positions[position] = 1
+                    positions[new_position] = 1
+            position += steps
         elif direction == 'L':
-            for _ in range(steps):
-                position -= 1
-                if position in positions:
-                    positions[position] += 1
+            for i in range(1, steps + 1):
+                new_position = position - i
+                if new_position in positions:
+                    positions[new_position] += 1
                 else:
-                    positions[position] = 1
-    
+                    positions[new_position] = 1
+            position -= steps
+
     overlapped_count = sum(1 for pos in positions if positions[pos] >= 2)
-    
+
     return overlapped_count
 
 # 입력 받기
