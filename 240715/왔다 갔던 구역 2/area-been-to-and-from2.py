534621ef6fc94484b9b34ce0ce643aecdf101960
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-07-15 13:47:34 +0900

    Title: 왔다 갔던 구역 2, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240715/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240715/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,35 +1,19 @@
-def count_overlapped_areas(n, commands):
-    position = 0
-    positions = {}
-
-    for command in commands:
-        steps, direction = command.split()
-        steps = int(steps)
-        
-        if direction == 'R':
-            for i in range(1, steps + 1):
-                new_position = position + i
-                if new_position in positions:
-                    positions[new_position] += 1
-                else:
-                    positions[new_position] = 1
-            position += steps
-        elif direction == 'L':
-            for i in range(1, steps + 1):
-                new_position = position - i
-                if new_position in positions:
-                    positions[new_position] += 1
-                else:
-                    positions[new_position] = 1
-            position -= steps
-
-    overlapped_count = sum(1 for pos in positions if positions[pos] >= 2)
-
-    return overlapped_count
-
-# 입력 받기
 n = int(input())
-commands = [input().strip() for _ in range(n)]
+state = 10  # 초기 위치를 10으로 설정
+arr = [0] * 100  # 위치를 기록할 배열, 충분히 큰 크기로 설정
 
-# 결과 출력
-print(count_overlapped_areas(n, commands))
+for _ in range(n):
+    cnt, direction = input().split()
+    cnt = int(cnt)
+    if direction == 'R':
+        for i in range(1, cnt + 1):
+            arr[state + i] += 1
+        state += cnt
+    elif direction == 'L':
+        for i in range(1, cnt + 1):
+            arr[state - i] += 1
+        state -= cnt
+
+# 2번 이상 지나간 영역의 크기 계산
+overlapped_count = sum(1 for x in arr if x >= 2)
+print(overlapped_count)
