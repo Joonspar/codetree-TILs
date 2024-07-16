2ed0ff9151ba254ea2800d9c3f2a0b14dcabe330
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-07-16 11:31:06 +0900

    Title: 신기한 타일 뒤집기, Time: 218ms, Memory: 40MB, Status: Wrong Answer - CodeTree

---
+++ 240716/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+arr = [0] * n * 1000
+current = n * 100
+
+for i in range(n):
+    x , directions = input().split()
+    x = int(x)
+    if directions == 'L':
+        for j in range(current, current - x, -1):
+            arr[j] = 'W'
+        current -= x
+    elif directions == 'R':
+        for j in range(current+1,current+x+1):
+            arr[j] = 'B'
+        current += x
+
+cntb = 0
+cntw = 0
+
+
+for i in range(len(arr)):
+    if arr[i] == 'W':
+        cntw += 1
+    elif arr[i] == 'B':
+        cntb += 1
+
+print(cntw, cntb)
