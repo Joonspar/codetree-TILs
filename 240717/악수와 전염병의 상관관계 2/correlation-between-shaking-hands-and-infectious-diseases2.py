232d0a4e229e64fcaace44e4f5da44a6ccf5b73a
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-07-17 14:46:44 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240717/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,24 @@
+def find_infected_developers(N, K, P, T, handshakes):
+    infected = [0] * N
+    handshake_count = [0] * N
+    
+    infected[P - 1] = 1
+    handshakes.sort()
+    
+    for t, x, y in handshakes:
+        x -= 1
+        y -= 1
+        
+        if infected[x] == 1 and handshake_count[x] < K:
+            infected[y] = 1
+            handshake_count[x] += 1
+        if infected[y] == 1 and handshake_count[y] < K:
+            infected[x] = 1
+            handshake_count[y] += 1
+    
+    return ''.join(map(str, infected))
+
+N, K, P, T = map(int, input().split())
+handshakes = [tuple(map(int, input().split())) for _ in range(T)]
+result = find_infected_developers(N, K, P, T, handshakes)
+print(result)
