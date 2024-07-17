4d44ad7cdb01d567d103c5bb970d3c2c85e9e2a2
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-07-17 19:42:50 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240717/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240717/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,26 +1,32 @@
 def find_infected_developers(N, K, P, T, handshakes):
     infected = [0] * N
-    handshake_count = [0] * N
-    
+    # handshake_count = [0] * N
+    cnt = 0
     infected[P - 1] = 1
-    handshakes.sort()
+    handshakes.sort() # 시간순으로 정렬
     
-    for t, x, y in handshakes:
+    for t, x, y in handshakes: # index를 0으로 시작하기 위함
         x -= 1
         y -= 1
         
-        if infected[x] == 1 and handshake_count[x] < K:
+        if infected[x] == 1:
             if infected[y] == 0:
                 infected[y] = 1
-                handshake_count[x] += 1
-        if infected[y] == 1 and handshake_count[y] < K:
+                cnt += 1
+                if cnt > K:
+                    break
+                # handshake_count[x] += 1
+        if infected[y] == 1:
             if infected[x] == 0:
                 infected[x] = 1
-                handshake_count[y] += 1
+                cnt += 1
+                if cnt > K:
+                    break
+                # handshake_count[y] += 1
     
     return ''.join(map(str, infected))
 
 N, K, P, T = map(int, input().split())
-handshakes = [tuple(map(int, input().split())) for _ in range(T)]
+handshakes = [tuple(map(int, input().split())) for _ in range(T)] # handshakes에 저장
 result = find_infected_developers(N, K, P, T, handshakes)
 print(result)
