1807eb50a39206a607c562805374a505bae1b3fe
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-07-17 16:08:16 +0900

    Title: 문자에 따른 명령 2, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240717/문자에 따른 명령 2/text-based-commands2.py
+++ 240717/문자에 따른 명령 2/text-based-commands2.py
@@ -1,32 +1,14 @@
-# 회전 , 방향 이동
-# 북쪽으로 향하고있음. 
-# 시계 방향 회전 L, 반대 방향 R
-dx,dy = [1,-1,0,0] , [0,0,-1,1]
-commands = input()
 x,y = 0,0
-dirn = 3
+dir_num = 0
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
 
-for i in range(len(commands)):
-    if commands[i] == 'L':
-        if dirn == 0:
-            dirn = 3 
-        elif dirn == 1:
-            dirn = 2
-        elif dirn == 2:
-            dirn = 0
-        else:
-            dirn = 1
-    elif commands[i] == 'R':
-        if dirn == 0:
-            dirn = 2
-        elif dirn == 1:
-            dirn = 3
-        elif drin == 2:
-            dirn = 1
-        else:
-            dirn = 0
-    elif commands[i] == 'F':
-        x = x + dx[dirn]
-        y = y + dy[dirn]
+for cmd in input():
+    if cmd == 'L':
+        dir_num = (dir_num-1) % 4
+    elif cmd == 'R':
+        dir_num = (dir_num+1) % 4 
+    else:
+        x , y = x+dx[dir_num] , y + dy[dir_num]
 
 print(x,y)
