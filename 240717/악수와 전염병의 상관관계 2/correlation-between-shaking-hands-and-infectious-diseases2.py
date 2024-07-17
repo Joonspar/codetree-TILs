40b8b07aa8b2c5e62b8c2e3c59b462c07fc481c4
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-07-17 19:57:04 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240717/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240717/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,32 +1,40 @@
 def find_infected_developers(N, K, P, T, handshakes):
+    # Initialize the infection status and handshake count for each developer
     infected = [0] * N
-    # handshake_count = [0] * N
-    cnt = 0
-    infected[P - 1] = 1
-    handshakes.sort() # 시간순으로 정렬
+    handshake_count = [0] * N
     
-    for t, x, y in handshakes: # index를 0으로 시작하기 위함
-        x -= 1
-        y -= 1
+    # Mark the initially infected developer
+    infected[P - 1] = 1
+    
+    # Sort the handshakes by time
+    handshakes.sort()
+    
+    # Process each handshake in order
+    for t, x, y in handshakes:
+        x -= 1  # Convert to 0-index
+        y -= 1  # Convert to 0-index
         
+        # Increase handshake count for infected developers
         if infected[x] == 1:
-            if infected[y] == 0:
-                infected[y] = 1
-                cnt += 1
-                if cnt > K:
-                    break
-                # handshake_count[x] += 1
+            handshake_count[x] += 1
         if infected[y] == 1:
-            if infected[x] == 0:
-                infected[x] = 1
-                cnt += 1
-                if cnt > K:
-                    break
-                # handshake_count[y] += 1
+            handshake_count[y] += 1
+        
+        # Check infection status and infect other developer if possible
+        if infected[x] == 1 and handshake_count[x] <= K:
+            infected[y] = 1
+        if infected[y] == 1 and handshake_count[y] <= K:
+            infected[x] = 1
     
     return ''.join(map(str, infected))
 
+# Input reading
 N, K, P, T = map(int, input().split())
-handshakes = [tuple(map(int, input().split())) for _ in range(T)] # handshakes에 저장
+handshakes = []
+for _ in range(T):
+    t, x, y = map(int, input().split())
+    handshakes.append((t, x, y))
+
+# Find the infected developers and print the result
 result = find_infected_developers(N, K, P, T, handshakes)
 print(result)
