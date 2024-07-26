b12d34e8e98ca51305a6406d3da2d936d26a9b89
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-07-26 23:06:28 +0900

    Title: G or H 3, Time: 342ms, Memory: 25MB, Status: Passed - CodeTree

--- 240726/G or H 3/G-or-H-3.py
+++ 240726/G or H 3/G-or-H-3.py
@@ -1,17 +1,21 @@
-n,k = map(int,input().split())
-arr = [0] * 10000
-# arr2 = []
-for i in range(n):
-    state,point = input().split()
+n, k = map(int, input().split())
+arr = [''] * 10001  # 위치가 1부터 10000까지이므로 크기를 10001로 설정
+
+for _ in range(n):
+    state, point = input().split()
     state = int(state)
     arr[state] = point
+
 max_cnt = 0
-for i in range(1,n-k+2):
+
+# 모든 시작 위치를 탐색
+for i in range(1, 10001 - k):
     res = 0
-    for j in range(i,i+k+1):
+    for j in range(i, i + k + 1):
         if arr[j] == 'G':
             res += 1
         elif arr[j] == 'H':
             res += 2
-    max_cnt = max(max_cnt,res)
+    max_cnt = max(max_cnt, res)
+
 print(max_cnt)
