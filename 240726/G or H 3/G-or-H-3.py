932a8734d4b895bde967b42955d395f222787d51
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-07-26 22:36:50 +0900

    Title: G or H 3, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240726/G or H 3/G-or-H-3.py
@@ -0,0 +1,18 @@
+n,k = map(int,input().split())
+arr = [0] * 10000
+# arr2 = []
+for i in range(n):
+    state,point = input().split()
+    state = int(state)
+    arr[state] = point
+max_cnt = 0
+for i in range(1,n-k+2):
+    res = 0
+    for j in range(i,i+k+1):
+        if arr[j] == 'G':
+            res += 1
+        elif arr[j] == 'H':
+            res += 2
+    max_cnt = max(max_cnt,res)
+# print(arr[0:17])
+print(max_cnt)
