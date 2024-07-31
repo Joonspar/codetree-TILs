682f7fed2a87fa573ce1fb59c342d6b48884fc60
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-07-31 14:31:45 +0900

    Title: 개발자의 능력 3, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240731/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,13 @@
+arr = list(map(int,input().split()))
+def get_diff(i,j,k):
+    first_team = arr[i] + arr[j] + arr[k]
+    second_team = sum(arr) - first_team
+    return abs(first_team - second_team)
+
+min_diff = 100
+for i in range(0,6):
+    for j in range(0,6):
+        for k in range(0,6):
+            min_diff = min(min_diff,get_diff(i,j,k))
+
+print(min_diff)
