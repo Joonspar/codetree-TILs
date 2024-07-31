55cd751a963e2a35aed0e63cd621090b08e145ed
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-07-31 16:39:12 +0900

    Title: 개발팀의 능력, Time: 99ms, Memory: 24MB, Status: Passed - CodeTree

--- 240731/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 240731/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -1,13 +1,19 @@
 arr = list(map(int,input().split()))
 min_diff = 1000
-def get_diff(i,j,k):
-    first = arr[i] + arr[j]
-    second = arr[k]
-    third = sum(arr) - first - second
-    return max(first,second,third) - min(first,second,third)
-
+error = False
 for i in range(5):
     for j in range(i+1,5):
-        for k in range(j+1,5):
-            min_diff = min(min_diff,get_diff(i,j,k))
-print(min_diff)
+        for k in range(5):
+            if k != i and k != j:
+                first = arr[i] + arr[j]
+                second = arr[k]
+                third = sum(arr) - first - second
+                if first != second and first != third and second != third:
+                    error = True
+                    diff = max(first,second,third) - min(first,second,third)
+                    min_diff = min(min_diff,diff)
+
+if error:
+    print(min_diff)
+else:
+    print(-1)
