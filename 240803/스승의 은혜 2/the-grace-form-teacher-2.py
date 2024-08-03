7ebeca5ef847e7e02ae606959639c582c29265d3
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-03 12:50:43 +0900

    Title: 스승의 은혜 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240803/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,19 @@
+n,b = map(int,input().split())
+cnt = 0
+p = []
+for _ in range(n):
+    price = int(input())
+    p.append(price)
+
+for i in range(n):
+    total = 0
+    cnt = 0
+    for j in range(n):
+        if i == j:
+            p[i] = p[i] // 2
+        total += p[j]
+        if total <= b:
+            cnt += 1
+        elif total > b:
+            p[i] *= 2
+print(cnt)
