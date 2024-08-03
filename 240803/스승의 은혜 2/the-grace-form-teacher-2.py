cf3002e04afd9a011458c2be9e3a6fc7862d3804
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-03 13:11:30 +0900

    Title: 스승의 은혜 2, Time: 178ms, Memory: 25MB, Status: Passed - CodeTree

--- 240803/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240803/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -1,21 +1,19 @@
 n,b = map(int,input().split())
-cnt = 0
 p = []
 count = [0] * n
 for _ in range(n):
     price = int(input())
     p.append(price)
-
+p.sort()
 for i in range(n):
     total = 0
-    cnt = 0
     for j in range(n):
         if i == j:
-            p[i] = p[i] // 2
-        total += p[j]
+            total += p[j] // 2
+        else:
+            total += p[j]
         if total <= b:
             count[i] += 1
         else:
-            p[i] *= 2
             break
 print(max(count))
