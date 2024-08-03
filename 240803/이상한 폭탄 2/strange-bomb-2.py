65c3accc83a4c27df9ba3507534fdf13a18c5228
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-03 15:19:38 +0900

    Title: 이상한 폭탄 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240803/이상한 폭탄 2/strange-bomb-2.py
+++ 240803/이상한 폭탄 2/strange-bomb-2.py
@@ -1,9 +1,9 @@
 n,k = map(int,input().split())
 bombs = []
+pair = []
 for i in range(n):
     num = int(input())
     bombs.append(num)
-
 bomb = False
 for i in range(n):
     for j in range(i+1,n):
@@ -12,9 +12,9 @@ for i in range(n):
                 bomb = True
                 a = i
                 b = j
+                pair.append([a,b])
                 break
-
 if bomb:
-    print(max(bombs[a:b+1]))
+    print(max(bombs[pair[0][0]:pair[0][1]]))
 else:
     print(-1)
