82ad08e7769963f972d56a1b4f5f9ebc1a44be4c
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-03 12:56:27 +0900

    Title: 스승의 은혜 2, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240803/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240803/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -1,13 +1,14 @@
 n,b = map(int,input().split())
 cnt = 0
 p = []
+count = []
 for _ in range(n):
     price = int(input())
     p.append(price)
 
 for i in range(n):
     total = 0
-    cnt = 1
+    cnt = 0
     for j in range(n):
         if i == j:
             p[i] = p[i] // 2
@@ -15,6 +16,7 @@ for i in range(n):
         if total <= b:
             cnt += 1
         else:
+            count.append(cnt)
             p[i] *= 2
             break
-print(cnt)
+print(max(count))
