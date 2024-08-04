db3126c751a1588beb47131f4a69ba37dbedc816
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-04 11:33:13 +0900

    Title: 스승의 은혜 3, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240804/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240804/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,24 +1,19 @@
 n, b = map(int, input().split())
-present = []
-ship = []
+cost = []
 for _ in range(n):
     a, c = map(int, input().split())
-    present.append(a)
-    ship.append(c)
-
-count = [0] * 101
+    cost.append([a,c])
+count = [0] * n
 
 for i in range(n):
     total = 0
     for j in range(n):
         if i == j:
-            total += (present[j] // 2) + ship[j]
+            total += (cost[j][0] // 2) + cost[j][1]
         else:
-            total += present[j] + ship[j]
-        
+            total += cost[j][0] + cost[j][1]
         if total <= b:
             count[i] += 1
         else:
-            break  # 무게 제한을 넘으면 더 이상 계산할 필요 없음
-
+            break
 print(max(count))
