6d547f74e979f0bc0200328618b1fc35e4ea9d67
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-04 14:02:30 +0900

    Title: 데이터센터의 온도 조정 2, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240804/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -0,0 +1,25 @@
+n,c,g,h = map(int,input().split())
+t_a = []
+t_b = []
+for i in range(n):
+    a,b = map(int,input().split())
+    t_a.append(a)
+    t_b.append(b)
+
+def section(l,r,t):
+    if t > l:
+        return c
+    elif l <= t and t <= r:
+        return g
+    else:
+        return h
+
+def total(t):
+    res = 0
+    for i in range(n):
+        res += section(t_a[i],t_b[i],t)
+    return res
+ans = 0
+for t in range(1,1001):
+    ans = max(ans,total(t))
+print(ans+1)
