f45d768752d09e006bd629f53f0242308b570fac
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-04 12:54:33 +0900

    Title: 스승의 은혜 3, Time: 174ms, Memory: 25MB, Status: Passed - CodeTree

--- 240804/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240804/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,21 +1,51 @@
-n, b = map(int, input().split())
-cost = []
-for _ in range(n):
-    a, c = map(int, input().split())
-    cost.append([a,c])
-count = []
-cost = sorted(cost,key = lambda x: sum(x))
-for i in range(n):
-    total = 0
-    cnt = 0
-    for j in range(n):
-        if i == j:
-            total += (cost[j][0] // 2) + cost[j][1]
+def calculate_total_cost(selected_students, students, coupon_index):
+    total_cost = 0
+    for i in range(len(selected_students)):
+        idx = selected_students[i]
+        if idx == coupon_index:
+            total_cost += (students[idx][0] // 2) + students[idx][1]  # 쿠폰 적용
         else:
-            total += cost[j][0] + cost[j][1]
-        if total <= b:
-            cnt += 1
-        else:
-            count.append(cnt)
-            break
-print(max(count))
+            total_cost += students[idx][0] + students[idx][1]  # 쿠폰 미적용
+    return total_cost
+
+def max_students(N, B, students):
+    # 학생을 선택할 수 있는 최대 수를 찾기 위해
+    max_count = 0
+    
+    for coupon_index in range(N):
+        # 쿠폰을 적용할 학생을 정하고, 나머지 학생들을 선택합니다.
+        costs = []
+        for i in range(N):
+            if i != coupon_index:
+                costs.append(students[i][0] + students[i][1])
+        costs.sort()
+        
+        # 쿠폰을 적용한 학생의 비용을 별도로 처리
+        coupon_cost = (students[coupon_index][0] // 2) + students[coupon_index][1]
+        
+        total_cost = coupon_cost
+        count = 1  # 쿠폰 적용된 학생 포함
+        
+        # 비용이 예산 내에서 가능한 최대 학생 수를 계산
+        for cost in costs:
+            if total_cost + cost <= B:
+                total_cost += cost
+                count += 1
+            else:
+                break
+        
+        max_count = max(max_count, count)
+    
+    return max_count
+
+# 입력 처리
+import sys
+input = sys.stdin.read
+data = input().split()
+
+N = int(data[0])
+B = int(data[1])
+students = [(int(data[2 + 2*i]), int(data[3 + 2*i])) for i in range(N)]
+
+# 결과 계산 및 출력
+print(max_students(N, B, students))
