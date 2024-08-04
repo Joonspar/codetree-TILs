e661d09ca0b01d22736ba8202142698645d65e3f
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-04 13:06:35 +0900

    Title: 숫자들의 합 중 최대, Time: 156ms, Memory: 25MB, Status: Passed - CodeTree

--- 240804/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
+++ 240804/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
@@ -2,7 +2,9 @@ x,y = map(int,input().split())
 res = []
 for i in range(x,y+1):
     s = 0
-    d1,d2 = map(int,list(str(i)))
-    s = d1 + d2
+    i = str(i)
+    for elem in i:
+        elem = int(elem)
+        s += elem
     res.append(s)
 print(max(res))
