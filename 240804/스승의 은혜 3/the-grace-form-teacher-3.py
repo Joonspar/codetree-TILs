9440e9ae602a2494c757fbac2c577c541f37301d
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-04 12:37:18 +0900

    Title: 스승의 은혜 3, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240804/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240804/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -3,17 +3,19 @@ cost = []
 for _ in range(n):
     a, c = map(int, input().split())
     cost.append([a,c])
-count = [0] * n
-cost.sort()
+count = []
+cost = sorted(cost,key = lambda x: sum(x))
 for i in range(n):
     total = 0
+    cnt = 0
     for j in range(n):
         if i == j:
             total += (cost[j][0] // 2) + cost[j][1]
         else:
             total += cost[j][0] + cost[j][1]
         if total <= b:
-            count[i] += 1
+            cnt += 1
         else:
+            count.append(cnt)
             break
 print(max(count))
