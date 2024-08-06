5e818be5e80a9ad44ddd53c0032b2435e6b25163
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-06 14:46:27 +0900

    Title: 야바위, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240806/야바위/ya-rock.py
+++ 240806/야바위/ya-rock.py
@@ -1,16 +1,18 @@
 n = int(input())
+games = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
 cnts = []
-for i in range(n):
-    a,b,c = map(int,input().split())
-    cups = [0] * 3
-    cups[i] = 1
+for i in range(3):
     cnt = 0
-    for j in range(n):
-        temp = cups[a-1]
-        cups[a-1] = cups[b-1]
-        cups[b-1] = temp
-        if cups[c-1] == 1:
+    cups = [0] * 3
+    for tries in games:
+        cups[i] = 1
+        temp = cups[tries[0]-1]
+        cups[tries[0]-1] = cups[tries[1]-1]
+        cups[tries[1]-1] = temp
+        if cups[tries[2]-1] == 1:
             cnt += 1
     cnts.append(cnt)
-
-print(cnts.index(max(cnts)))
+print(cnts.index(max(cnts))+1)
