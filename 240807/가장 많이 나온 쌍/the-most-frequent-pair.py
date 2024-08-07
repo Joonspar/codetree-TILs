ec534b5d7f5c9e911bb31f1d4d84f1a07b2c3abe
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-07 14:59:03 +0900

    Title: 가장 많이 나온 쌍, Time: 143ms, Memory: 28MB, Status: Passed - CodeTree

--- 240807/가장 많이 나온 쌍/the-most-frequent-pair.py
+++ 240807/가장 많이 나온 쌍/the-most-frequent-pair.py
@@ -6,7 +6,7 @@ sections = [
 ans = 0
 cnts = []
 for i in range(1,n+1):
-    for j in range(1,n+1):
+    for j in range(i+1,n+1):
         cnt = 0
         for k in range(m):
             if i in sections[k] and j in sections[k]:
