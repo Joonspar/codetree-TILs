e52de4f7038eaaa885aea3bb34a4dd91e8a0034b
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-07 13:57:37 +0900

    Title: 가장 작은 x 찾기, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240807/가장 작은 x 찾기/find-smallest-x.py
@@ -0,0 +1,19 @@
+n = int(input())
+num = [
+    tuple(map(int,input().split()))
+    for _ in range(n)
+]
+correct = False
+ans = 1000
+for x in range(1,n+1):
+    for a,b in num:
+        x *= 2
+        if a <= x <= b:
+            correct = True
+        else:
+            correct = False
+            break
+    if correct:
+        x //= (2**n)
+        ans = min(x,ans)
+print(ans)
