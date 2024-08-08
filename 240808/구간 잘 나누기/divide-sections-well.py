5d304a11718f4dd26fe50b2db4241053089ca0f6
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-08 20:54:20 +0900

    Title: 구간 잘 나누기, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

--- 240808/구간 잘 나누기/divide-sections-well.py
+++ 240808/구간 잘 나누기/divide-sections-well.py
@@ -1,17 +1,35 @@
-n, m = map(int, input().split())
-arr = list(map(int, input().split()))
+def is_possible(mid, numbers, n, m):
+    count = 1
+    current_sum = 0
+    
+    for num in numbers:
+        if current_sum + num > mid:
+            count += 1
+            current_sum = num
+            if count > m:
+                return False
+        else:
+            current_sum += num
+    
+    return True
 
-# 합으로 가능한 최소값을 limit으로 지정
-for limit in range(1, 100 * 100 + 1):
-    # arr의 앞에서부터 탐색
-    prev_i = 0
-    partition = 0
-    for i in range(n):
-        if limit < sum(arr[prev_i: i + 1]):
-            partition += 1
-            prev_i = i
-            
-    if partition == m - 1:
-        limit = max(limit, max(arr)) # 구간은 나눠진 상태
-        print(limit)
-        break
+def minimize_max_partition_sum(n, m, numbers):
+    left, right = max(numbers), sum(numbers)
+    result = right
+    
+    while left <= right:
+        mid = (left + right) // 2
+        if is_possible(mid, numbers, n, m):
+            result = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return result
+
+# 입력 받기
+n, m = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+# 결과 출력
+print(minimize_max_partition_sum(n, m, numbers))
