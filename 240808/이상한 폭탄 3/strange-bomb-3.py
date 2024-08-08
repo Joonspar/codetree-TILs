c9456e7be3c7c0aa8e54f8e97c3ea7c2acda0903
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-08 16:43:18 +0900

    Title: 이상한 폭탄 3, Time: 151ms, Memory: 101MB, Status: Wrong Answer - CodeTree

---
+++ 240808/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,14 @@
+n,k = map(int,input().split())
+bombs = []
+for _ in range(n):
+    bombs.append(int(input()))
+max_bombs = [0] * 10000000
+for i in range(n):
+    cnt = 0
+    for j in range(i+1,n):
+        if bombs[i] == bombs[j]:
+            cnt += 1
+    max_bombs[bombs[i]] += cnt
+ans = max(max_bombs)
+print(max_bombs[0:8])
+print(max_bombs.index(ans))
