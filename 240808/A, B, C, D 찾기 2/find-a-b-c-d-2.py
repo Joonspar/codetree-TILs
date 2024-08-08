a340fda0e88f6240db1bb6580f796982df208677
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-08 14:28:25 +0900

    Title: A, B, C, D 찾기 2, Time: 229ms, Memory: 25MB, Status: Passed - CodeTree

--- 240808/A, B, C, D 찾기 2/find-a-b-c-d-2.py
+++ 240808/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -1,64 +1,19 @@
-def getResult(idx):
+MAX_A = 40
+MAX_N = 15
 
-    # 1. 기저조건
-    if len(temp) >= 1:
-        sum_temp = sum(temp)
-        if sum_temp not in dict_check:
-            dict_check[sum_temp] = 1
-        else:
-            dict_check[sum_temp] += 1
-
-    # 2. 백트래킹
-    for i in range(idx, 4):
-        temp.append(temp_arr[i])
-        getResult(i+1)
-        temp.pop()
-
-def check(dict_c, dict_a):
-
-    # 1. 딕셔너리 비교
-    for check in dict_c:
-        
-        # 1.1
-        if check not in dict_a:
-            return False
-
-        # 1.2
-        if dict_c[check] != dict_a[check]:
-            return False
+# 변수 선언 및 입력
+arr = list(map(int, input().split()))
     
-    return True
-    
-# 입력
-import sys
-input = sys.stdin.readline
-
-arr = list(map(int,input().split()))
-
-# 계산1: 조건 딕셔너리
-dict_arr = {}
-for ele in arr:
-    if ele not in dict_arr:
-        dict_arr[ele] = 1
-    else:
-        dict_arr[ele] += 1
-
-# 계산2 및 출력
-# : a+b+c+d <= 40
-# : 가능한 답은 유일 
-for a in range(1, 37+1):
-    for b in range(1, 37+1):
-        for c in range(1, 37+1):
-            for d in range(1, 37+1):
+# 모든 a, b, c, d를 확인해서
+# 이 합들이 arr과 같은지 여부를 확인합니다.
+for a in range(1, MAX_A + 1):
+    for b in range(a, MAX_A + 1):
+        for c in range(b, MAX_A + 1):
+            for d in range(c, MAX_A + 1):
+                arr2 = [a, b, c, d, a + b, b + c, c + d, d + a,
+                    a + c, b + d, a + b + c, a + b + d, a + c + d, b + c + d,
+                    a + b + c + d]
                 
-                # 1. 체크 딕셔너리 생성(백트래킹_오름차순)
-                dict_check = {}
-                temp_arr = [a,b,c,d]
-
-                temp = []
-                getResult(0)
-
-                # 2. 딕셔너리 비교
-                if check(dict_check, dict_arr):
-                    print(a,b,c,d)
-                    quit()
+                if sorted(arr) == sorted(arr2):
+                    # 만약 두 배열이 완전히 같다면 a, b, c, d조합을 찾아냈습니다.
+                    print(a, b, c, d)
