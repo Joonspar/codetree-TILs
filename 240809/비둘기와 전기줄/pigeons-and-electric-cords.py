ec6bb023b58a5d8a1127ac05418134cc4ac641be
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-09 16:43:30 +0900

    Title: 비둘기와 전기줄, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240809/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -0,0 +1,17 @@
+n = int(input())
+sections = [
+    list(input().split())
+    for _ in range(n)
+]
+cnt = 0
+for i in range(n):
+    extra = 101
+    for j in range(i+1,n):
+        sections[i][0] = int(sections[i][0])
+        sections[j][0] = int(sections[j][0])
+        if sections[i][0] == sections[j][0]:
+            if sections[i][1] != sections[j][1]:
+                cnt += 1
+            sections[j][0] += extra
+            extra += 1
+print(cnt)
