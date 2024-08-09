62780f331d6dd57303448edfef23d36531f5ab19
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-09 13:18:59 +0900

    Title: 최대 H 점수 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240809/최대 H 점수 2/maximum-h-score-2.py
+++ 240809/최대 H 점수 2/maximum-h-score-2.py
@@ -1,5 +1,3 @@
-from itertools import combinations
-
 def calculate_h_score(arr):
     arr.sort(reverse=True)
     h_score = 0
@@ -11,20 +9,19 @@ def calculate_h_score(arr):
     return h_score
 
 def max_h_score_after_operations(n, l, arr):
+    arr.sort(reverse=True)
     max_h = calculate_h_score(arr)
     
     if l == 0:
         return max_h
     
-    unique_values = sorted(set(arr))
-    
-    for indices in combinations(unique_values, min(l, len(unique_values))):
-        modified_arr = arr[:]
-        for value in indices:
-            for i in range(n):
-                if modified_arr[i] == value:
-                    modified_arr[i] += 1
-        max_h = max(max_h, calculate_h_score(modified_arr))
+    for _ in range(l):
+        # 현재 배열에서 증가시키면 가장 효과적인 원소 선택
+        for i in range(n):
+            if arr[i] > arr[-1]: # 가장 작은 원소는 증가해도 H 점수에 영향 없음
+                arr[i] += 1
+                break
+        max_h = max(max_h, calculate_h_score(arr))
     
     return max_h
 
