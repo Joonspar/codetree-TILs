af16516c0671760221a1cabf5fe4efbc3040ce8f
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-09 15:25:43 +0900

    Title: 구역 청소, Time: 153ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/구역 청소/district-cleaning.py
+++ 240809/구역 청소/district-cleaning.py
@@ -2,9 +2,9 @@ a,b = map(int,input().split())
 c,d = map(int,input().split())
 cnt = [0] * 100
 
-for i in range(a,b+1):
+for i in range(a,b):
     cnt[i] += 1
-for i in range(c,d+1):
+for i in range(c,d):
     cnt[i] += 1
 new_list = [x for x in cnt if x>=1]
-print(len(new_list)-1)
+print(len(new_list))
