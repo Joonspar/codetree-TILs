5b372fe67fb193c90c8b8cf9575638bbf8e86c98
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-09 13:05:41 +0900

    Title: 최대 H 점수 2, Time: 5094ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

--- 240809/최대 H 점수 2/maximum-h-score-2.py
+++ 240809/최대 H 점수 2/maximum-h-score-2.py
@@ -1,15 +1,18 @@
-n,l = map(int,input().split())
-arr = list(map(int,input().split()))
+from itertools import combinations
+
+n, l = map(int, input().split())
+arr = list(map(int, input().split()))
+
 ans = 0
-for h in range(101):
-    cnt = 0
-    for k in range(n):
-        for _ in range(l):
-            arr[k] += l
-    for j in range(n):
-        if arr[j] >= h:
-            cnt += 1
-        if cnt == h:
-            ans = max(cnt,h)
-        arr[k] -= l
+
+for h in range(101): 
+    for indices in combinations(range(n), l): 
+        original = arr[:]  
+        for idx in indices:
+            arr[idx] += 1  
+        cnt = sum(1 for x in arr if x >= h) 
+        if cnt >= h:  
+            ans = max(ans, h)  
+        arr = original 
+
 print(ans)
