5e376da9c3dac9509ca62a23733eef2857d38008
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-09 14:52:16 +0900

    Title: 두 직사각형, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240809/두 직사각형/two-rectangles.py
@@ -0,0 +1,6 @@
+x1,y1,x2,y2 = map(int,input().split())
+a1,b1,a2,b2 = map(int,input().split())
+if (x1 <= a1 and a1 >= x2) or (x1>=a1 and x1 <= a2) or (y1 >= b1 and y1 <= b2) or (b1>=y1 and y1 <= b2):
+    print('overlapping')
+else:
+    print('nonoverlapping')
