60ea93a8794184f71bdfb5f3bdeec9e7d82b2e9b
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-11 13:09:11 +0900

    Title: X 달리기, Time: 157ms, Memory: 25MB, Status: Passed - CodeTree

--- 240811/X 달리기/x-run.py
+++ 240811/X 달리기/x-run.py
@@ -1,26 +1,26 @@
-def min_time_to_reach(X):
-    time = 0  # 걸린 시간
-    distance = 0  # 이동한 거리
-    speed = 1  # 현재 속도
-
-    while True:
-        # 현재 속도로 이동하고 시간 증가
-        distance += speed
-        time += 1
-
-        # 거리 체크
-        if distance >= X and speed == 1:
-            break
-        
-        # 다음 스텝에서 속도를 줄여야 한다면 줄이고, 아니라면 증가
-        if distance + speed + 1 <= X:
-            speed += 1
-        else:
-            speed -= 1
-    
-    return time
-
-# 입력 받기
 X = int(input())
-# 결과 출력
-print(min_time_to_reach(X)-1)
+
+curr_pos = 0
+curr_speed = 1
+sum_time = 0
+while (True):
+    curr_pos += curr_speed
+    sum_time += 1
+    curr_speed += 1
+    increase_sum = 0
+
+    if (curr_pos == X):
+        break
+    
+    for i in range(curr_speed):
+        increase_sum += i
+    if X - (curr_pos + curr_speed) < increase_sum:
+        curr_speed -= 1
+    
+    stay_sum = 0
+    for i in range(curr_speed):
+        stay_sum += i
+    if X - (curr_pos + curr_speed) < stay_sum:
+        curr_speed -= 1
+
+print(sum_time)
