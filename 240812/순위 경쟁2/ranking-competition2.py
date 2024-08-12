6da469f2c6d2320ae32da26478fd072f11bc4814
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-12 15:31:07 +0900

    Title: 순위 경쟁2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240812/순위 경쟁2/ranking-competition2.py
+++ 240812/순위 경쟁2/ranking-competition2.py
@@ -3,14 +3,14 @@ games = [
     list(input().split())
     for _ in range(n)
 ]
-cnta , cntb , fame = 0,0,1
+cnta , cntb , fame = 0,0,0
 
 for game in games:
     game[1] = int(game[1])
     if game[0] == 'A':
         cnta += game[1]
+        fame += 1
     elif game[0] == 'B':
         cntb += game[1]
-        fame += 1
 
 print(fame)
