7526dd36cad9997e88e02e7c3df686320c4bd139
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-12 16:03:40 +0900

    Title: 두 번째로 작은 수의 위치, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
+++ 240812/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
@@ -3,8 +3,8 @@ arr = list(map(int,input().split()))
 new_arr = sorted(arr)
 ans = -1
 for i in range(n-1):
-    if arr[i] != arr[i+1]:
-        ans = arr[i+1]
+    if new_arr[i] != new_arr[i+1]:
+        ans = new_arr[i+1]
         break
 
 if arr.count(ans) < 1 or arr.count(ans) >= 2:
