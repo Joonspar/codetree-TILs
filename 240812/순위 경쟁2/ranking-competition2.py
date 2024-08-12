cb3d62a1d0d3aaa0040d84b25968e8d191c0b073
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-12 15:47:50 +0900

    Title: 순위 경쟁2, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/순위 경쟁2/ranking-competition2.py
+++ 240812/순위 경쟁2/ranking-competition2.py
@@ -4,13 +4,24 @@ games = [
     for _ in range(n)
 ]
 cnta , cntb , fame = 0,0,0
+current_hall_of_fame = {"A", "B"}
 
 for game in games:
     game[1] = int(game[1])
     if game[0] == 'A':
         cnta += game[1]
-        fame += 1
+
     elif game[0] == 'B':
         cntb += game[1]
-
+    
+    if cnta > cntb:
+        new_hall_of_fame = {"A"}
+    elif cntb > cnta:
+        new_hall_of_fame = {"B"}
+    else:
+        new_hall_of_fame = {"A", "B"}
+    
+    if new_hall_of_fame != current_hall_of_fame:
+        fame += 1
+        current_hall_of_fame = new_hall_of_fame
 print(fame)
