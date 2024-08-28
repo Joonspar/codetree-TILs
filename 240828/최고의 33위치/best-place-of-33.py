4b306084713fb9da000d3b6841283477591a58aa
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-28 17:49:21 +0900

    Title: 최고의 33위치, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240828/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,14 @@
+n = int(input())
+rect = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+ans = 0
+for i in range(n-2):
+    cnt = 0
+    for j in range(i,i+3):
+        for k in range(i,i+3):
+            if rect[j][k] == 1:
+                cnt += 1
+    ans = max(ans,cnt)
+print(ans)
