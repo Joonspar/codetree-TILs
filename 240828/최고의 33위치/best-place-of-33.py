ad55dceda6c5825b8da03261aef922ad7aab6792
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-28 17:52:05 +0900

    Title: 최고의 33위치, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240828/최고의 33위치/best-place-of-33.py
+++ 240828/최고의 33위치/best-place-of-33.py
@@ -5,10 +5,11 @@ rect = [
 ]
 ans = 0
 for i in range(n-2):
-    cnt = 0
-    for j in range(i,i+3):
-        for k in range(i,i+3):
-            if rect[j][k] == 1:
-                cnt += 1
-    ans = max(ans,cnt)
+    for l in range(n-2):
+        cnt = 0
+        for j in range(i,i+3):
+            for k in range(l,l+3):
+                if rect[j][k] == 1:
+                    cnt += 1
+        ans = max(ans,cnt)
 print(ans)
