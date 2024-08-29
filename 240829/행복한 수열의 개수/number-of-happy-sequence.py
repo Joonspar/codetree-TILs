2dcca5f52ad3ab6c2e6005c766c8c8564c05628d
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-29 12:37:00 +0900

    Title: 행복한 수열의 개수, Time: 169ms, Memory: 25MB, Status: Passed - CodeTree

--- 240829/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240829/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,31 +1,47 @@
-n,m = map(int,input().split())
+n, m = map(int,input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
 
-arr = [
-    list(map(int,input().split()))
-    for _ in range(n)
-]
-ans = 0
+def row_happy(x):
+    check_happy_num = arr[x][0]
+    
+    max_cnt = 0
+    cnt = 0
+    for i in range(1,n):
+        if check_happy_num == arr[x][i]:
+            cnt += 1            
+            max_cnt = max(max_cnt,cnt)
+        else:
+            check_happy_num = arr[x][i]            
+            max_cnt = max(max_cnt,cnt)
+            cnt = 0
+    
+    return max_cnt
 
+def cul_happy(x):
+    check_happy_num = arr[0][x]
+
+    max_cnt = 0
+    cnt = 0
+    for i in range(1,n):
+        
+        if check_happy_num == arr[i][x]:
+            cnt += 1
+            max_cnt = max(max_cnt,cnt)
+        else:
+            check_happy_num = arr[i][x]
+            max_cnt = max(max_cnt,cnt)
+            cnt = 0
+        
+    
+    return max_cnt
+
+num_happy = 0
 for i in range(n):
-    cnt = 1    
-    for j in range(1,n):
-        if arr[i][j-1] == arr[i][j]:
-            cnt += 1
-        else:
-            cnt = 1
-        if cnt >= m:
-            ans += 1
-            break
-    for k in range(1,n):
-        if arr[k-1][i] == arr[k][i]:
-            cnt += 1
-        else:
-            cnt = 1
-        if cnt >= m:
-            ans += 1
-            break
+    row_happy_num, cul_happy_num = row_happy(i), cul_happy(i)
+    #print(row_happy_num, cul_happy_num)
+    if row_happy_num >= m-1:
+        num_happy += 1
+    if cul_happy_num >= m-1:
+        num_happy += 1  
 
-if n == 1:
-    print(2)
-else:
-    print(ans)
+print(num_happy)
