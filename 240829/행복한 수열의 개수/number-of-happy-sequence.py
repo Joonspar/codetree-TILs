6a505a501fdc48ca6716bfdf5a77fc17400d2830
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-08-29 12:32:35 +0900

    Title: 행복한 수열의 개수, Time: 118ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240829/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240829/행복한 수열의 개수/number-of-happy-sequence.py
@@ -11,12 +11,16 @@ for i in range(n):
     for j in range(1,n):
         if arr[i][j-1] == arr[i][j]:
             cnt += 1
+        else:
+            cnt = 1
         if cnt >= m:
             ans += 1
             break
     for k in range(1,n):
         if arr[k-1][i] == arr[k][i]:
             cnt += 1
+        else:
+            cnt = 1
         if cnt >= m:
             ans += 1
             break
