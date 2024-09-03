197980e3b3c10ccec2adf0c2b4d42a58c15f5da3
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-09-03 13:46:36 +0900

    Title: 첫 번째와 두 번째 교환, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240903/첫 번째와 두 번째 교환/exchange-1st-and-2nd.py
+++ 240903/첫 번째와 두 번째 교환/exchange-1st-and-2nd.py
@@ -1,8 +1,18 @@
-s = list(input())
-for i in range(len(s)):
-	if s[i] == 'c':
-		s[i] = 'o'
-		i += 1
-	elif s[i] == 'o':
-		s[i] = 'c'
-print(*s,sep='')
+def swap_chars(s):
+    first_char = s[0]
+    second_char = s[1]
+    
+    result = ''
+    for char in s:
+        if char == first_char:
+            result += second_char
+        elif char == second_char:
+            result += first_char
+        else:
+            result += char
+    
+    return result
+
+# 예제 입력
+s = "codctrcc"
+print(swap_chars(s))  # "ocdotroo"
