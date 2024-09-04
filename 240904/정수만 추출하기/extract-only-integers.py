7d003a9c61ce3146f5b3b2fa014871ec2e8a9ddf
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-09-04 20:36:38 +0900

    Title: 정수만 추출하기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240904/정수만 추출하기/extract-only-integers.py
@@ -0,0 +1,22 @@
+s1,s2 = input().split()
+l1,l2 = len(s1),len(s2)
+ans = []
+z = ''
+for i in range(l1):
+    if '0' <= s1[i] <='9':
+        z += s1[i]
+    else:
+        ans.append(z)
+        break
+z= ''
+for i in range(l2):
+    if '0' <= s2[i] <= '9':
+        z += s2[i]
+    else:
+        ans.append(z)
+        break
+
+su = 0
+for i in range(len(ans)):
+    su += int(ans[i])
+print(su)
