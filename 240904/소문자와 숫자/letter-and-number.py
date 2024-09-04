72a4ccc4478eb66e1ac3314b62dad5d22daf64e2
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-09-04 19:56:55 +0900

    Title: 소문자와 숫자, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240904/소문자와 숫자/letter-and-number.py
+++ 240904/소문자와 숫자/letter-and-number.py
@@ -3,6 +3,6 @@ ans = []
 s = s.lower()
 
 for i in range(len(s)):
-    if 'a' <= s[i] <= 'z' or '1' <= s[i] <= '9':
+    if 'a' <= s[i] <= 'z' or '0' <= s[i] <= '9':
         ans.append(s[i])
 print(*ans,sep='')
