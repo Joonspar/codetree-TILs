514d4e14208edd4208efb421ecb9950f1d4e6c85
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-09-08 14:50:16 +0900

    Title: 별표 출력하기 11, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240908/별표 출력하기 11/print-star-11.py
+++ 240908/별표 출력하기 11/print-star-11.py
@@ -3,7 +3,7 @@ n = int(input())
 for i in range(2*n+1):
     for j in range(2*n+1):
         if (i % 2 == 0) or (j % 2 == 0):
-            print('* '*(2*n+1),end='')
+            print('* ',end='')
         else:
-            print(' ',end='')
-        print()
+            print('  ',end='')
+    print()
