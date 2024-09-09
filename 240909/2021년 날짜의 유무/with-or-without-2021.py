bc83577d2c37cb3c8f72e68ead5378c88866a5be
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-09-09 19:21:57 +0900

    Title: 2021년 날짜의 유무, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240909/2021년 날짜의 유무/with-or-without-2021.py
+++ 240909/2021년 날짜의 유무/with-or-without-2021.py
@@ -4,13 +4,17 @@ def check(m,d):
     if m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12:
         if 1 <= d <= 31:
             return 'Yes'
+        else:
+            return 'No'
     elif m == 4 or m == 6 or m == 9 or m == 11:
         if 1 <= d <= 30:
             return 'Yes'
+        else:
+            return 'No'
     elif m == 2:
         if 1 <= d <= 28:
             return 'Yes'
-    else:
-        return 'No'
+        else:
+            return 'No'
 
 print(check(m,d))
