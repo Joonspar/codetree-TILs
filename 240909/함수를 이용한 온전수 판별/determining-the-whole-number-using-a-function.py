3b463f2486a325417d2469c6028b2b1852f88364
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-09-09 19:06:46 +0900

    Title: 함수를 이용한 온전수 판별, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240909/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -0,0 +1,9 @@
+a,b = map(int,input().split())
+
+def func(a,b):
+    cnt = 0
+    for i in range(a,b+1):
+        if i % 2 != 0 and (i%10 != 5) and (i%3 != 0 or i % 9 == 0):
+            cnt += 1
+    return cnt
+print(func(a,b))
