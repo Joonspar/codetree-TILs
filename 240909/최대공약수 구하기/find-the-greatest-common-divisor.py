e6a4075747d79f90d3b206e99d4074dce052bb63
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-09-09 14:45:58 +0900

    Title: 최대공약수 구하기, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240909/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,9 @@
+n,m = map(int,input().split())
+def gmd(n,m):
+    ans = 1
+    for i in range(1,min(n,m)):
+        if n % i == 0 and m % i == 0:
+            res = i
+        ans = max(ans,res)
+    return ans
+print(gmd(n,m))
