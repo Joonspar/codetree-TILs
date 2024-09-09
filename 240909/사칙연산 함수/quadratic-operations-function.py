9e4bc24dada6d78dee92ece66d009ebb55600740
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-09-09 19:03:00 +0900

    Title: 사칙연산 함수, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240909/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,24 @@
+a,o,c = input().split()
+def add(a,c):
+    a,c = int(a),int(c)
+    print(f'{a} + {c} = {a+c}')
+def diff(a,c):
+    a,c = int(a),int(c)
+    print(f'{a} - {c} = {a-c}')
+def div(a,c):
+    a,c = int(a),int(c)
+    print(f'{a} // {c} = {a//c}')
+def mul(a,c):
+    a,c = int(a),int(c)
+    print(f'{a} * {c} = {a*c}')
+
+if o == '+':
+    add(a,c)
+elif o == '-':
+    diff(a,c)
+elif o == '/':
+    div(a,c)
+elif o == '*':
+    mul(a,c)
+else:
+    print('False')
