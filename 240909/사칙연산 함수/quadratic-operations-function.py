79c93a701862b4b132f6c2ccaf4373d98c58ed5d
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-09-09 19:03:30 +0900

    Title: 사칙연산 함수, Time: 112ms, Memory: 25MB, Status: Passed - CodeTree

--- 240909/사칙연산 함수/quadratic-operations-function.py
+++ 240909/사칙연산 함수/quadratic-operations-function.py
@@ -7,7 +7,7 @@ def diff(a,c):
     print(f'{a} - {c} = {a-c}')
 def div(a,c):
     a,c = int(a),int(c)
-    print(f'{a} // {c} = {a/c}')
+    print(f'{a} / {c} = {a//c}')
 def mul(a,c):
     a,c = int(a),int(c)
     print(f'{a} * {c} = {a*c}')
