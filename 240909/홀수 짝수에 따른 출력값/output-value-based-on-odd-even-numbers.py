8df13478164b44fd0b9eae26d97d918e0c3b02d8
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-09-09 21:20:00 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 138ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240909/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+def func(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    
+    if n % 2 == 0:
+        return n + func(n-2)
+    else:
+        return n + func(n-2)
+
+print(func(n))
