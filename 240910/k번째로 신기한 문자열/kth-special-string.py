0f692ce3f5be274fca42efd1fc9241dd7990322f
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-09-10 13:18:24 +0900

    Title: k번째로 신기한 문자열, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240910/k번째로 신기한 문자열/kth-special-string.py
+++ 240910/k번째로 신기한 문자열/kth-special-string.py
@@ -1,15 +1,17 @@
 n,k,T = input().split()
 n,k = int(n),int(k)
+
+def check(a,b):
+    if len(a) < len(b):
+        return False
+    return a[:len(b)] == b
+
 li = []
-for _ in range(n):
+
+for i in range(n):
     s = input()
-    same = False
-    for j in range(len(T)):
-        if s[j] == T[j]:
-            same = True
-        else:
-            break
-    if same:
+    if check(s,T):
         li.append(s)
+
 li.sort()
-print(li[k])
+print(li[k-1])
