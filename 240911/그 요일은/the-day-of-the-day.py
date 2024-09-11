a663ca3207dab1798add66ffbb78b8444782524d
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-09-11 15:25:35 +0900

    Title: 그 요일은, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240911/그 요일은/the-day-of-the-day.py
+++ 240911/그 요일은/the-day-of-the-day.py
@@ -10,4 +10,10 @@ for i in range(1,m1):
     day1 -= nums[i]
 day1 -= d1
 cnt = 0
-print(day1//7 + 1)
+start_idx = 0
+target_idx = date.index(A)
+for i in range(day1+1):  # 시작일과 종료일 포함
+    current_idx = (start_idx + i) % 7  # 현재 날짜의 요일 인덱스
+    if current_idx == target_idx:
+        cnt += 1
+print(cnt)
