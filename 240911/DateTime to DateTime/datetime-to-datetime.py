b2bff2aa25d9bc8c73c060bca4ee7bb169b5b41e
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-09-11 14:34:21 +0900

    Title: DateTime to DateTime, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240911/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,14 @@
+a,b,c = map(int,input().split())
+o_d , o_h , o_m = 11,11,11
+total = 0
+total += 1440*(a-o_d)
+if o_m >= c:
+    total += (60*(b-o_h-1) + (60-abs(o_m-c))) 
+else:
+    total += (60*(b-o_h) + c-o_m)
+
+if b <= 11:
+    if c < 11:
+        print(-1)
+else:
+    print(total)
