7fa55c94671f9e24077efd33749c98d9e489a3e2
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-09-13 17:14:06 +0900

    Title: 왔다 갔던 구역 2, Time: 122ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240913/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240913/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,19 +1,26 @@
 n = int(input())
 res = [0] * 2000
-cur = 1001
+cur = 1001  # 시작점은 1001 (OFFSET 포함)
+
 for _ in range(n):
-    dis,dire = input().split()
+    dis, dire = input().split()
     dis = int(dis)
+    
     if dire == 'R':
-        for i in range(cur,cur+dis):
+        # 오른쪽으로 이동: cur + 1부터 cur + dis까지 칠하기
+        for i in range(cur + 1, cur + dis + 1):
             res[i] += 1
-        cur += dis
+        cur += dis  # 이동 후 위치 갱신
     else:
-        for i in range(cur,cur-dis,-1):
+        # 왼쪽으로 이동: cur - 1부터 cur - dis까지 칠하기
+        for i in range(cur - 1, cur - dis - 1, -1):
             res[i] += 1
-        cur -= dis
+        cur -= dis  # 이동 후 위치 갱신
+
+# 2번 이상 지나간 영역의 크기 구하기
 cnt = 0
 for i in range(len(res)):
     if res[i] >= 2:
         cnt += 1
+
 print(cnt)
