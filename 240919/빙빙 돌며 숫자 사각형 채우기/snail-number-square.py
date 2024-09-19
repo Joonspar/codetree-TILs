dd6531e1625b508dd09b36586351af9a7931e378
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-09-19 13:39:20 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 171ms, Memory: 25MB, Status: Passed - CodeTree

--- 240919/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 240919/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -7,12 +7,12 @@ rects = [
     [0 for _ in range(m)]
     for _ in range(n)
 ]
-x , y = 0,0
-rects[0][0] = 1
-dir_num = 0
 dxs , dys = [0,1,0,-1] , [1,0,-1,0]
+x , y = 0,0
+dir_num = 0
+rects[x][y] = 1
 def in_range(x,y):
-    return 0<= x < m and 0<= y < n
+    return 0<= x < n and 0<= y < m
 
 for i in range(2,n*m+1):
     nx,ny = x + dxs[dir_num] , y + dys[dir_num]
