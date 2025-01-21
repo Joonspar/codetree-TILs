486685fb4ceed19bdfdb1b2215ecdb4a9194c245
박준서 <166091275+Joonspar@users.noreply.github.com>
2025-01-21 16:59:19 +0900

    Title: 세 실수의 반올림, Time: 157ms, Memory: 12MB, Status: Passed - CodeTree

--- 250121/세 실수의 반올림/rounding-of-three-actual-numbers.java
+++ 250121/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
-        double c = sc.nextDouble();
-        System.out.printf("%.3f",a\n);
-        System.out.printf("%.3f",b\n);
-        System.out.printf("%.3f",c);
+
+        // 변수 선언
+        double a, b, c;
+
+        // 입력
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        c = sc.nextDouble();
+
+        // 출력
+        System.out.printf("%.3f\n%.3f\n%.3f", a, b, c);
     }
 }
