1d233b59389fd3ab80a0bf7d6d2a446f6bbf8db7
박준서 <166091275+Joonspar@users.noreply.github.com>
2025-01-21 11:43:29 +0900

    Title: 입력받아 계산, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 250121/입력받아 계산/input-calculate.java
+++ 250121/입력받아 계산/input-calculate.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String args[]) {
         Scanner sc = new Scanner(System.in);
-        int a;
-        a = sc.nextInt();
-        System.out.println(a);
+        int a = sc.nextInt();
+        System.out.println(a+2);
     }
 }
