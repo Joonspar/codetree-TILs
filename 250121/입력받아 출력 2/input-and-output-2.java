6ab7929bd777b43ffc33a45f93b52af4007187e6
박준서 <166091275+Joonspar@users.noreply.github.com>
2025-01-21 22:53:47 +0900

    Title: 입력받아 출력 2, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250121/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        int s1 = sc.nextInt();
+        int s2 = sc.nextInt();
+
+        System.out.println(s1+""+s2);
+    }
+}
