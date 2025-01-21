a043251ccae0c7667d52ccf29d0e90f4fad3184c
박준서 <166091275+Joonspar@users.noreply.github.com>
2025-01-21 11:58:59 +0900

    Title: 입력받아 계산 4, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250121/입력받아 계산 4/input-calculate-4.java
+++ 250121/입력받아 계산 4/input-calculate-4.java
@@ -1,9 +1,8 @@
-import.java.util.Scanner;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        a *= 2;
         System.out.println(a);
     }
 }
