1c4f8e24653f52f64276e5c90712de931f96b21e
박준서 <166091275+Joonspar@users.noreply.github.com>
2025-01-21 11:49:23 +0900

    Title: 입력받아 계산 4, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250121/입력받아 계산 4/input-calculate-4.java
+++ 250121/입력받아 계산 4/input-calculate-4.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.println(a*2);
+        a *= 2;
+        System.out.println(a);
     }
 }
