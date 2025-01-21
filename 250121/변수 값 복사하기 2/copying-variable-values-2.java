a78da455c54a54e20f0e872cb360a2a43af2bdb4
박준서 <166091275+Joonspar@users.noreply.github.com>
2025-01-21 11:00:29 +0900

    Title: 변수 값 복사하기 2, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250121/변수 값 복사하기 2/copying-variable-values-2.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2 , b = 3 , c = 7;
+        a = b = c;
+        System.out.println(a + " " + b + " " + c);
+    }
+}
