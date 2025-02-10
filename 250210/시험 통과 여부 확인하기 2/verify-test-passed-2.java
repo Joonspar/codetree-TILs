8dfcabf9c5653a9e4cb076db492171bae8240d05
박준서 <166091275+Joonspar@users.noreply.github.com>
2025-02-10 12:00:41 +0900

    Title: 시험 통과 여부 확인하기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250210/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -0,0 +1,24 @@
+imort java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] score = new int[4];
+        int cnt = 0 , sum = 0;
+        for (int i = 0; i<n; i++){
+            for (int j = 0; i<4; j++){
+                score[j] = sc.nextInt();
+                sum += score;
+            }
+            int avg = sum / 4;
+            if (avg >= 60){
+                System.out.println("pass");
+                cnt += 1;
+            }else{
+                System.out.println("fail");
+            }
+        }
+        System.out.println(cnt);
+    }
+}
